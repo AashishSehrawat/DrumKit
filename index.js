3c4edbbbe4389d09b141ebd6d41a8a16cdbe9528
AashishSehrawat <132571077+AashishSehrawat@users.noreply.github.com>
2024-02-03 12:11:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+for(var i=0; i<document.getElementsByClassName("drum").length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function() {
+        
+        var buttonHtml = this.innerHTML;
+        makeSound(buttonHtml);
+        animateButton(buttonHtml);
+    });
+}
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    animateButton(event.key);
+
+});
+
+function makeSound(key){
+    switch(key){
+        case 'w':
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+        break;
+
+        case 'a':
+            var audio = new Audio("./sounds/tom-2.mp3")
+            audio.play();
+        break;
+
+        case 's':
+            var audio = new Audio("./sounds/tom-3.mp3")
+            audio.play();
+        break;
+
+        case 'd':
+            var audio = new Audio("./sounds/tom-4.mp3")
+            audio.play();
+        break;
+
+        case 'j':
+            var audio = new Audio("./sounds/snare.mp3")
+            audio.play();
+        break;
+
+        case 'k':
+            var audio = new Audio("./sounds/crash.mp3")
+            audio.play();
+        break;
+
+        case 'l':
+            var audio = new Audio("./sounds/kick-bass.mp3")
+            audio.play();
+        break;
+
+        default:
+            console.log(this.innerHTML);
+
+    }
+}
+
+
+function animateButton(currentButton){
+    var activeButton = document.querySelector("."+currentButton);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    } , 100);
+
+}
+
